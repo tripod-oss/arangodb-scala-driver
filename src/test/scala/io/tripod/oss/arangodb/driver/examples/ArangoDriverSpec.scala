42079c906017031eac437990e5d733b48d75b864
njouanin <nico@beerfactory.org>
2017-01-23 14:38:23 +0100

    Try fix

--- src/test/scala/io/tripod/oss/arangodb/driver/examples/ArangoDriverSpec.scala
+++ src/test/scala/io/tripod/oss/arangodb/driver/examples/ArangoDriverSpec.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import io.tripod.oss.arangodb.driver.{ArangoDriver, ServerVersionResponse}
 import io.tripod.oss.arangodb.driver.database.DatabaseApi
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.{EitherValues, Matchers, WordSpec}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 
 /**
@@ -14,6 +14,7 @@ class ArangoDriverSpec
     extends WordSpec
     with Matchers
     with ScalaFutures
+    with EitherValues
     with IntegrationPatience {
   implicit val system = ActorSystem("PbfStreamFlowSpec")
   implicit val materializer = ActorMaterializer()
@@ -23,8 +24,9 @@ class ArangoDriverSpec
     "get server version" in {
       val driver = new ArangoDriver() with DatabaseApi
       val result = driver.getServerVersion(true).futureValue
-      result.isRight shouldBe true
-      result.map(response => response.server shouldEqual "arango")
+      result.right.value.server should not be empty
+      result.right.value.version should not be empty
+      result.right.value.license should not be empty
     }
   }
 }
